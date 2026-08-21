0808615b1682be0f84584ce6630cec50d2d695ba
Noobish-Procyon <justin.mo313@education.nsw.gov.au>
2026-08-21 14:43:23 +1000

    Delete tbs directory

--- tbs/saveSystem.js
+++
@@ -1,9 +0,0 @@
-function saveGame(state) {
-  localStorage.setItem("circleClashSave", JSON.stringify(state));
-}
-
-function loadGame() {
-  const raw = localStorage.getItem("circleClashSave");
-  if (!raw) return null;
-  return JSON.parse(raw);
-}
--- tbs/utils.js
+++
@@ -1,9 +0,0 @@
-function randInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function dist(a, b) {
-  const dx = a.x - b.x;
-  const dy = a.y - b.y;
-  return Math.sqrt(dx*dx + dy*dy);
-}
